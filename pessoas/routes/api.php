07b7b6da7f0c2ef494d0417951f0b5848fb71209
ledracaio <caioaugustoledra@gmail.com>
2024-11-07 17:29:07 -0300

    vai

--- pessoas/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-use Illuminate\Support\Facades\Route;
-
-Route::post('/login', 'AuthController@login');
-Route::post('/register', 'AuthController@register');
-Route::resource('pessoas', 'PessoaController');
-
-?>
